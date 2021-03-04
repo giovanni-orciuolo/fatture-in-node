c69a4eaacbcd82c4a092cb2399b33920f07a8d51
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2021-03-04 13:00:13 +0100

    Setup for publish

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Utility classes
+export { acquisti } from "./routes/acquisti";
+export { anagrafica } from "./routes/anagrafica";
+export { corrispettivi } from "./routes/corrispettivi";
+export { documenti } from "./routes/documenti";
+export { info } from "./routes/info";
+export { magazzino } from "./routes/magazzino";
+export { mail } from "./routes/mail";
+export { prodotti } from "./routes/prodotti";
+export { richiesta } from "./routes/richiesta";
+
+// The main class of the library
+export { FattureInCloud } from "./lib/fatture-in-cloud";
